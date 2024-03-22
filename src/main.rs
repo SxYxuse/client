7f01a279f5d13c00908ccea2639f8b6662007b46
Vandendijck <m.vandendijck@student.helmo.be>
2024-03-22 09:36:51 +0100

    Refactor main UX

--- src/main.rs
+++ src/main.rs
@@ -9,49 +9,59 @@ use std::io::{self, Write};
 use crate::client::unicast_handler::UnicastHandler;
 
 fn main() {
-    let handler = UnicastHandler::new("192.168.133.5", 60001);
+    let handler = UnicastHandler::new("192.168.133.2", 60001);
 
     loop {
         let mut input = String::new();
         println!("Menu:");
-        println!("1. Add service....:  NEWMON <augmented_url>");
-        println!("2. List services..:  LISTMON");
-        println!("3. Check status...:  REQUEST <id>");
-        println!("4. Quit...........:  QUIT");
+        println!("1. Add service");
+        println!("2. List services");
+        println!("3. Check status");
+        println!("4. Quit");
         print!("Enter command: ");
         io::stdout().flush().unwrap();
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
-                let parts: Vec<&str> = input.trim().split_whitespace().collect();
-                if parts.is_empty() {
-                    continue;
-                }
-                //PRENDRE EN COMPTE LES MINISCULE A L ENTRE DE L UTILISATEUR
-                match parts[0] {
-                    "NEWMON" => {
-                        if parts.len() < 2 {
-                            println!("Usage: NEWMON <augmented_url>");
-                            continue;
+                let command: i32 = input.trim().parse().unwrap_or(0);
+                match command {
+                    1 => {
+                        print!("Enter augmented_url: ");
+                        io::stdout().flush().unwrap();
+                        let mut url = String::new();
+                        match io::stdin().read_line(&mut url) {
+                            Ok(_) => {
+                                let message = handler.build_newmon_message(url.trim());
+                                let response = handler.send_message(&message).unwrap();
+                                print!("Response from monitor: {}", response)
+                            }
+                            Err(e) => {
+                                eprintln!("Erreur lors de la lecture de l'entrée de l'utilisateur: {}", e);
+                                continue;
+                            }
                         }
-                        let message = handler.build_newmon_message(parts[1]);
-                        let response = handler.send_message(&message).unwrap();
-                        print!("Response from monitor: {}", response)
                     }
-                    "LISTMON" => {
+                    2 => {
                         let message = handler.build_listmon_message();
                         let response = handler.send_message(&message).unwrap();
                         println!("Response from monitor: {}", response);
                     }
-                    "REQUEST" => {
-                        if parts.len() < 2 {
-                            println!("Usage: REQUEST <id>");
-                            continue;
+                    3 => {
+                        print!("Enter id: ");
+                        io::stdout().flush().unwrap();
+                        let mut id = String::new();
+                        match io::stdin().read_line(&mut id) {
+                            Ok(_) => {
+                                let message = handler.build_request_message(id.trim());
+                                let response = handler.send_message(&message).unwrap();
+                                println!("Response from monitor: {}", response)
+                            }
+                            Err(e) => {
+                                eprintln!("Erreur lors de la lecture de l'entrée de l'utilisateur: {}", e);
+                                continue;
+                            }
                         }
-                        let message = handler.build_request_message(parts[1]);
-                        let reponse = handler.send_message(&message).unwrap();
-                        println!("Response from monitor: {}", reponse)
                     }
-                    "QUIT" => {
+                    4 => {
                         println!("Exiting program.");
                         break;
                     }
